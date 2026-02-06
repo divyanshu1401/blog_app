5ebad6b086144d3e2cbb1136e82a4f7d4062cdc6
Divyanshu Sharma <76512800+divyanshu1401@users.noreply.github.com>
2026-02-06 18:28:04 +0530

    add functionality to attach photos to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  has_one_attached :image
+
   def liked_by?(user)
     likes.exists?(user: user)
   end
