9263e61f27637adb3b21e60e02d72ab53ca59e5c
Divyanshu Sharma <76512800+divyanshu1401@users.noreply.github.com>
2026-02-10 00:57:53 +0530

    Fix storage.yml prefix configuration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+
+  before_create :prefix_image_key
+
   belongs_to :user
 
   has_many :comments, dependent: :destroy
@@ -8,5 +11,11 @@ class Post < ApplicationRecord
 
   def liked_by?(user)
     likes.exists?(user: user)
+  end
+
+  def prefix_image_key
+    if image.attached? && !image.blob.key.start_with?('uploads/')
+      image.blob.update(key: "uploads/#{image.blob.key}")
+    end
   end
 end
