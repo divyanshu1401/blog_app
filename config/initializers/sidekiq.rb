fa2897ed61b65e5aaf8a41a92d478ab47b7d5643
Divyanshu Sharma <76512800+divyanshu1401@users.noreply.github.com>
2026-02-11 15:55:39 +0530

    update sidekiq config to fix crosslot error

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -18,9 +18,15 @@ else
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }
+  config.redis = { 
+    url: redis_url,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }
+  config.redis = { 
+    url: redis_url,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
